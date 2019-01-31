35ba22b1632bfe83085a278e3b67fd00f311f55f
Douglas Ahlquist <douglas@Douglass-MacBook-Pro.local>
2019-01-31 10:27:55 -0800

    Add estore_transactrion project to main maven build and Opps renamed transaction project

--- estore_transaction/src/main/java/com/ahlquist/estore/transaction/controller/TrsnsactionController.java
+++ estore_transaction/src/main/java/com/ahlquist/estore/transaction/controller/TrsnsactionController.java
@@ -18,12 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ahlquist.estore.services.UserService;
+import com.ahlquist.estore.services.TransactionService;
 
 
-@RestController("userController")
+@RestController("transactionController")
 @RequestMapping(value = "/api/v1/transaction")
 public class TrsnsactionController {
+	
+	final static Logger logger = Logger.getLogger(TrsnsactionController.class);
+	final static String ERROR = "error";
+	
+	
+	@Autowired
+	@Qualifier("transactionService")
+	TransactionService transactionService;
 	
 	/**
 	 * 
@@ -33,10 +41,10 @@ public class TrsnsactionController {
 	@RequestMapping(method = { RequestMethod.POST }, consumes = { "application/json;charset=UTF-8" }, produces = {
 			"application/json;charset=UTF-8", "text/plain" })
 	@ResponseBody
-	public ResponseEntity<String> userCreate(@RequestBody Map<String, String> map) {
+	public ResponseEntity<String> transactionCreate(@RequestBody Map<String, String> map) {
 
 		logger.debug("user: " + map.toString());
-		userService.create(map);
+		transactionService.create(map);
 
 		return new ResponseEntity<String>("success", HttpStatus.OK);
 	}
